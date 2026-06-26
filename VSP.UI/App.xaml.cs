d19df76f593d596abb8bebcdd52b474fd813c94a
game2082001 <game2082001@hotmail.com>
2026-06-27 02:23:57 +0800

    Sprint 5.4 - UI Framework

--- VSP.UI/App.xaml.cs
+++ VSP.UI/App.xaml.cs
@@ -1,14 +1,7 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
-namespace VSP.UI
+namespace VSP.UI;
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
-
 }
