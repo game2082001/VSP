394a2a5f1459c90cdad13ac7f9df8695557390ca
game2082001 <game2082001@hotmail.com>
2026-06-26 21:57:03 +0800

    Sprint 3.3：Navigation

---
+++ VSP.Core/Navigation/INavigationService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VSP.Core.Navigation
+{
+    internal class INavigationService
+    {
+    }
+}
---
+++ VSP.UI/Views/DashboardView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace VSP.UI.Views;
+
+public partial class DashboardView : UserControl
+{
+    public DashboardView()
+    {
+        InitializeComponent();
+    }
+}
